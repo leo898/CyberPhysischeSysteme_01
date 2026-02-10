1b730aad93d4ae9102b3ee40b8f40a164fcdcc2d
leo898 <leonard@keil-gruenberg.de>
2026-02-10 08:20:00 +0100

    config

--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-MENU = {
-    "espresso": {
-        "price": 8.80,
-        "needs": {"water_ml": 50, "milk_ml": 100, "coffee_g": 15}
-    },
-    "cappuccino": {
-        "price": 5.20,
-        "needs": {"water_ml": 50, "milk_ml": 200, "coffee_g": 15} 
-    },
-    "latte": {
-        "price": 4.80,
-        "needs": {"water_ml": 0, "milk_ml": 250, "coffee_g": 25} 
-    }
-}
-
-ACCEPTED_COINS = [0.05, 0.10, 0.20, 1.00, 2.00]
-
-resources = {
-    "water_ml": 150,
-    "milk_ml": 300,
-    "coffee_g": 75,
-    "money_eur": 35.50
-}
+from config import resources, MENU, ACCEPTED_COINS
 
 # ---------------------
 # Hilfsfunktionen
@@ -218,6 +196,7 @@ def main():
         make_drink(choice)
         
         deduct_ingredients(choice)
+        print("Zutaten wurden aktualisiert. Zurück zur Startsituation...\n")
 
 if __name__ == "__main__":
     main()
